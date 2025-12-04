1d3019e9679c1444e88016317b6ceef0444acd29
Benjamin Malamuth <bmalamut@asu.edu>
2025-12-04 09:36:03 -0700

    finished creating initial search path

--- category_search.py
+++ category_search.py
@@ -1,10 +1,16 @@
 from duckduckgo_search import DDGS
 
+from api_tools import call_model_chat_completions
+from fallback_question import get_fallback_answer
+
 search_system_prompt = '''
     You are an expert researching agent. You have access to a search engine. Given a question, you will return a query to search for relevant information. Return only what you want to search.
 '''
 fallback_search_system_prompt = '''
     You are an expert researching agent. You have access to a search engine. Given a question and an error from a previous search, you will return a corrected query to search for relevant information. Return only what you want to search.
+'''
+final_system_prompt = '''
+    You are an expert researching agent. You will be given a question and results from a search engine to help you answer the question. Use the search results to produce a final answer to the question. Output only the final answer.
 '''
 
 def web_search(query: str) -> tuple(bool, str):
@@ -24,3 +30,30 @@ def web_search(query: str) -> tuple(bool, str):
 
     except Exception as e:
         return False, str(e)
+
+def answer_question(question: str, query: str, result: tuple(bool, str), max_searches = 5) -> str:
+    for _ in range(max_searches):
+
+        if not result[0]:
+            new_query = call_model_chat_completions(
+                prompt = f"Question: {question}\nPrevious Query: {query}\nError: {result[1]}\nProvide a new query to search for relevant information.",
+                system = fallback_search_system_prompt
+            )
+            query = new_query
+            result = web_search(query)
+
+        return call_model_chat_completions(
+            prompt = f"Question: {question}\nSearch Results:\n{result[1]}\nProvide a final answer to the question based on the search results.",
+            system = final_system_prompt
+        )
+
+    return get_fallback_answer(question)
+
+def search_question(question: str) -> str:
+    query = call_model_chat_completions(
+        prompt = f"Question: {question}\nProvide a query to search for relevant information.",
+        system = search_system_prompt
+    )
+    result = web_search(query)
+
+    return answer_question(question=question, query=query, result=result)
